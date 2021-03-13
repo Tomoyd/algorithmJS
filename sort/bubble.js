63a6b3821d7d94cf996f441ff9cc8e56f8fe7d93
Tomoyd <ydh9419@163.com>
2021-03-13 23:53:18 +0800

    recursion

--- sort/bubble.js
+++ sort/bubble.js
@@ -12,7 +12,7 @@
 
 function bubbleSort(numbers) {
   let len = numbers.length;
-  let tmp;
+  let tmp = 0;
   for (let i = 0; i < len - 1; i++) {
     for (let j = 0; j < len - 1 - i; j++) {
       tmp = numbers[j];
