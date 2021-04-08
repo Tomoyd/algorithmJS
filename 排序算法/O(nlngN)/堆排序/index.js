c0929c24acc883da8f69d576c53f824095acbca1
Tomoyd <ydh9419@163.com>
2021-04-08 23:26:22 +0800

    [NFF]堆排序

--- 排序算法/O(nlngN)/堆排序/index.js
+++ 排序算法/O(nlngN)/堆排序/index.js
@@ -1,38 +1,43 @@
 /**
  * 堆排序
  */
-const adjustHeap = (arr, parentIndex, length) => {
-  let temp = arr[parentIndex]; /* temp保存父节点值，用于最后赋值 */
-  let childIndex = 2 * parentIndex + 1; /* 保存子节点位置 */
-  while (childIndex < length) {
-    /* 如果有右子节点，且右子节点大于左子节点的值，则定位到右子节点 */
-    if (childIndex + 1 < length && arr[childIndex + 1] > arr[childIndex]) {
-      childIndex++;
-    }
-    /* 如果父节点小于任何一个子节点的值，直接退出循环 */
-    if (temp >= arr[childIndex]) {
-      break;
-    }
-    /* 无序交换，单向赋值即可 */
-    arr[parentIndex] = arr[childIndex];
-    parentIndex = childIndex;
-    childIndex = 2 * childIndex + 1;
+
+function heapify(arr, current, n) {
+  let left = 2 * current + 1;
+  let right = 2 * current + 2;
+  let max = current;
+  if (left < n && arr[left] > arr[max]) {
+    max = left;
   }
-  arr[parentIndex] = temp;
-};
-const heapSort = (arr) => {
-  /* 把无序数列构建成最大堆 */
-  for (let i = Math.floor(arr.length / 2); i >= 0; --i) {
-    adjustHeap(arr, i, arr.length - 1);
+  if (right < n && arr[right] > arr[max]) {
+    max = right;
   }
-  for (let i = arr.length - 1; i > 0; --i) {
-    /* 交换最后一个元素与第一个元素 */
+  if (max === current) {
+    return;
+  }
+  let temp = arr[max];
+  arr[max] = arr[current];
+  arr[current] = temp;
+
+  heapify(arr, max, n);
+}
+
+function buildHeap(arr, n) {
+  for (let i = Math.floor((n - 2) / 2); i >= 0; i--) {
+    heapify(arr, i, n);
+  }
+}
+
+function heapSort(arr) {
+  buildHeap(arr, arr.length);
+
+  for (let i = arr.length - 1; i >= 1; i--) {
     [arr[i], arr[0]] = [arr[0], arr[i]];
-    /* 调整最大堆 */
-    adjustHeap(arr, 0, i);
+    heapify(arr, 0, i);
   }
   return arr;
-};
+}
 
-const sorted = heapSort([1, 3, 4]);
-console.log("sorted :>> ", sorted);
+const heapSorted = heapSort([1, 2, 5, 7, 9, 9, 2]);
+
+console.log("heapSorted :>> ", heapSorted);
